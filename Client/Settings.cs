4c1f656cbefc724eb647ab95782a8e9d120f8b0e
pyq <pyq225@gmail.com>
2019-12-07 18:12:16 +0800

    update

--- Client/Settings.cs
+++ Client/Settings.cs
@@ -73,6 +73,7 @@ namespace Client
 
         //Network
         public static bool UseConfig = false;
+        //public static string IPAddress = "192.168.31.60";
         public static string IPAddress = "127.0.0.1";
         public static int Port = 7000;
         public const int TimeOut = 5000;
